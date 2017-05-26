a05b1d4be2d0d2c847295feadfe1c4e5704236af
Suresh Kaukuntly <suresh.kaukuntly@costrategix.com>
2017-05-26 13:33:55 +0530

    test

---
+++ src/main/java/com/knolbee/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.knolbee.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
